aa75072a5bb5c2e535c01971126ff4385c771ee1
bkapo <bkapo@outlook.com>
2018-01-24 23:09:02 +0200

    npm update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full'},
+  { path: '', redirectTo: 'search', pathMatch: 'full'},
   { path: 'search', loadChildren: '../app/search/search.module#SearchModule' },
   { path: 'iplist', loadChildren: '../app/involved-party/involved-party.module#InvolvedPartyModule' }
 ];
